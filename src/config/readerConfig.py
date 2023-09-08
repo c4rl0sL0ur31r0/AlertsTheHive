c7ff38b19cb48a98ca58c2495c5d56542fca56bd
CarlosLoureiro <59171494+c4rl0sL0ur31r0@users.noreply.github.com>
2023-09-08 18:20:36 +0200

    Update readerConfig.py

--- src/config/readerConfig.py
+++ src/config/readerConfig.py
@@ -5,7 +5,7 @@ class readerConfig:
 
     def __init__(self):
         self.parse = ConfigParser()
-        self.parse.read(r'C:\Users\ender\Desktop\c1b3rwallTaller23\src\config\config.ini')
+        self.parse.read('src\config\config.ini')
         # self.parse.read('../config.ini')
 
     def get_thehive_config(self):
@@ -13,6 +13,3 @@ class readerConfig:
         :return: hive.url, hive.api config
         """
         return self.parse['thehive']
-
-#data = readerConfig().get_thehive_config()
-#print(data['hive.url'], data['hive.api'])
